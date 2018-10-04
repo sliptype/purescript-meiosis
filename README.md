326524fab21710dbd562aebba99dcc1259bf51fc
Sebastian Klingler <sklingler93@gmail.com>
2018-10-03 20:20:47 -0500

    Grab the document

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # purescript-meiosis
 A purescript implementation of the [Meiosis pattern](https://github.com/foxdonut/meiosis)
 
+# Development
+This project uses `purs 0.12.0` and `psc-package`.
+
+Run the dev server with
+```
+pulp server
+```
+
--- src/Main.purs
+++ src/Main.purs
@@ -3,7 +3,14 @@ module Main where
 import Prelude
 import Effect (Effect)
 import Effect.Console (log)
+import Effect.Class (liftEffect)
+import Web.HTML (window)
+import Web.HTML.Window (document)
+import Web.HTML.HTMLDocument (toDocument)
+import Web.DOM (Element)
+import Web.DOM.Document (url)
 
 main :: Effect Unit
 main = do
-  log "Hello sailor!"
+  doc <- liftEffect $ toDocument <$> (document =<< window)
+  log =<< url doc
