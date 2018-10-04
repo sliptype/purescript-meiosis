3fdb2398c2321b936dca91552d462b0f45966aec
Sebastian Klingler <sklingler93@gmail.com>
2018-10-03 20:41:14 -0500

    Grab root element

--- src/Main.purs
+++ src/Main.purs
@@ -4,13 +4,19 @@ import Prelude
 import Effect (Effect)
 import Effect.Console (log)
 import Effect.Class (liftEffect)
+import Data.Maybe (Maybe(..))
 import Web.HTML (window)
 import Web.HTML.Window (document)
 import Web.HTML.HTMLDocument (toDocument)
 import Web.DOM (Element)
-import Web.DOM.Document (url)
+import Web.DOM.Document (Document, toNonElementParentNode, url)
+import Web.DOM.Element (id)
+import Web.DOM.NonElementParentNode (getElementById)
 
 main :: Effect Unit
 main = do
   doc <- liftEffect $ toDocument <$> (document =<< window)
-  log =<< url doc
+  elementResult <- getElementById "app" $ toNonElementParentNode doc
+  case elementResult of
+    Just element -> log =<< id element
+    Nothing -> log "Element not found"
