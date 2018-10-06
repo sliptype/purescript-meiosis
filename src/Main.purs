fc2f4b48a7fab8ee0fac39f84683d5db196ef358
Sebastian Klingler <sklingler93@gmail.com>
2018-10-05 21:31:58 -0500

    Add snabbdom

--- src/Main.purs
+++ src/Main.purs
@@ -2,21 +2,18 @@ module Main where
 
 import Prelude
 import Effect (Effect)
-import Effect.Console (log)
-import Effect.Class (liftEffect)
 import Data.Maybe (Maybe(..))
-import Web.HTML (window)
-import Web.HTML.Window (document)
-import Web.HTML.HTMLDocument (toDocument)
-import Web.DOM (Element)
-import Web.DOM.Document (Document, toNonElementParentNode, url)
-import Web.DOM.Element (id)
-import Web.DOM.NonElementParentNode (getElementById)
+import Data.Map (empty)
+import Snabbdom (VNodeProxy, h, patchInitialSelector, text, toVNodeEventObject, toVNodeHookObjectProxy)
+
+createVNode :: String ->  VNodeProxy
+createVNode message = h "strong#msg"
+  { attrs : empty
+  , on : toVNodeEventObject empty
+  , hook : toVNodeHookObjectProxy { insert : Nothing, update : Nothing, destroy : Nothing}
+  }
+  [text message]
 
 main :: Effect Unit
 main = do
-  doc <- liftEffect $ toDocument <$> (document =<< window)
-  elementResult <- getElementById "app" $ toNonElementParentNode doc
-  case elementResult of
-    Just element -> log =<< id element
-    Nothing -> log "Element not found"
+  patchInitialSelector "#app" $ createVNode "Hello world"
