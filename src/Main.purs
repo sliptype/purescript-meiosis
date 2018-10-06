d6dd29767b37cfbcb44157cee060955708722438
Sebastian Klingler <sklingler93@gmail.com>
2018-10-05 22:06:35 -0500

    Add view function

--- src/Main.purs
+++ src/Main.purs
@@ -3,17 +3,27 @@ module Main where
 import Prelude
 import Effect (Effect)
 import Data.Maybe (Maybe(..))
-import Data.Map (empty)
-import Snabbdom (VNodeProxy, h, patchInitialSelector, text, toVNodeEventObject, toVNodeHookObjectProxy)
+import Data.Map (empty, singleton)
+import Snabbdom (VNodeProxy, VNodeData, h, patchInitialSelector, text, toVNodeEventObject, toVNodeHookObjectProxy)
 
-createVNode :: String ->  VNodeProxy
-createVNode message = h "strong#msg"
+type State = Int
+
+emptyVNodeData :: VNodeData
+emptyVNodeData =
   { attrs : empty
   , on : toVNodeEventObject empty
   , hook : toVNodeHookObjectProxy { insert : Nothing, update : Nothing, destroy : Nothing}
   }
-  [text message]
+
+initialState :: State
+initialState = 10
+
+view :: State -> VNodeProxy
+view s = h "div" emptyVNodeData
+  [ h "strong#msg" emptyVNodeData [text $ "Counter: " <> (show s)]
+  , h "button" emptyVNodeData [text "Increase"]
+  ]
 
 main :: Effect Unit
 main = do
-  patchInitialSelector "#app" $ createVNode "Hello world"
+  patchInitialSelector "#app" $ view initialState
